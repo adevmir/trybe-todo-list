2da1dac448a9b3dc0ccd4ddaabda6928e5d02a10
adevmir <ademirvalonqar@gmail.com>
2022-02-19 14:06:17 -0300

    [Requisitos 7] Ao clicar em um item na lista o marca com fundo cinza e clicar em outro item marca o novo e desmarca o anterior.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+var itemnum = 1;
+var contClickList = 1;
+
 function criaTarefa(li) {
+    let item = 'item' + itemnum;  
     const lista = document.createElement(li);
     const textoTarefa = document.querySelector('#texto-tarefa').value;
     lista.innerText = textoTarefa;
+    lista.setAttribute("onclick", "fundoCinza('" + item + "')");
+    lista.setAttribute("id", item);
+    lista.setAttribute("class", 'item');
+    itemnum += 1;
 
     var create = document.querySelector('ol');
     create.appendChild(lista);
     document.querySelector('#texto-tarefa').value = '';
+}
+
+function fundoCinza(a) {
+  if (contClickList > 1) { 
+    const itemAnterior = document.querySelector(".selected")
+    itemAnterior.setAttribute("class", "item");
+    itemAnterior.setAttribute("style", 'background-color: white;');
+  }
+    const itemList = document.getElementById(a);
+    itemList.setAttribute("style", "background-color: gray;");
+    itemList.setAttribute("class", "selected");
+    contClickList += 1;
 }
